bf5928801ba42ffa97a5363660d958c15e50f89f
Tianyang Liu <lty0711@qq.com>
2016-12-08 22:20:49 -0800

    added sql
    haven't tried

--- data/starbucks.sql
+++ data/starbucks.sql
@@ -20,37 +20,142 @@ SET time_zone = "+00:00";
 -- Database: `starbucks`
 --
 
+--
+-- Table structure for table `ci_sessions`
+--
+
+CREATE TABLE `ci_sessions` (
+  `id` varchar(128) NOT NULL,
+  `ip_address` varchar(45) NOT NULL,
+  `timestamp` int(10) UNSIGNED NOT NULL DEFAULT '0',
+  `data` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+---
+--- Table structure for table 'Inventories'
+---
+
 DROP TABLE IF EXISTS 'Inventories';
 CREATE TABLE 'Inventories' (
-    'id' varchar(11) NOT NULL,
+    'id' int NOT NULL AUTO_INCREMENT,
     'name' varchar(64) NOT NULL,
     'quantity' varchar(11) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
-ALTER TABLE `Inventories`
+ALTER TABLE 'Inventories'
   ADD PRIMARY KEY (`id`);
 
-INSERT INTO 'Inventories' ('id', 'name', 'quantity') VALUES
-(1, 'Milk', 1000),
-(2, 'Ice', 1000),
-(3, 'Sugar', 1000),
-(4, 'Water', 1000),
-(5, 'Banana', 1000),
-(6, 'Blueberry', 1000),
-(7, 'Apple', 1000),
-(8, 'Grape', 1000),
-(9, 'Orange', 1000),
-(10, 'Coconut Milk', 1000),
-(11, 'Honey', 1000),
-(12, 'Lime Refresher Base', 1000),
-(13, 'Berry Refresher Base', 1000),
-(14, 'Classic Syrup', 1000),
-(15, 'An Infusion Of', 1000),
-(16, 'Mocha Sauce', 1000),
-(17, 'Whipped Cream', 1000),
-(18, 'Vanilla Syrup', 1000),
-(19, 'Chili Mocha Powder', 1000),
-(20, 'Spiced Mocha Topping', 1000);
+INSERT INTO 'Inventories' ('name', 'quantity') VALUES
+('Milk', 1000),
+('Ice', 1000),
+('Sugar', 1000),
+('Water', 1000),
+('Banana', 1000),
+('Blueberry', 1000),
+('Apple', 1000),
+('Grape', 1000),
+('Orange', 1000),
+('Coconut Milk', 1000),
+('Honey', 1000),
+('Lime Refresher Base', 1000),
+('Berry Refresher Base', 1000),
+('Classic Syrup', 1000),
+('An Infusion Of', 1000),
+('Mocha Sauce', 1000),
+('Whipped Cream', 1000),
+('Vanilla Syrup', 1000),
+('Chili Mocha Powder', 1000),
+('Spiced Mocha Topping', 1000);
+
+
+---
+--- Table structure for table 'Menu'
+---
+
+DROP TABLE IF EXISTS 'Menu';
+CREATE TABLE 'Menu' (
+    'id' int NOT NULL AUTO_INCREMENT,
+    'name' varchar(64) NOT NULL,
+    'description' text NOT NULL,
+    'price' decimal(20,2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE 'Menu'
+  ADD PRIMARY KEY ('id');
+
+INSERT INTO 'Menu' ('name', 'description', 'price') VALUES
+('Caramel Macchiato', 'Freshly steamed milk with vanilla-flavored syrup is marked with espresso and topped with caramel drizzle for an oh-so-sweet finish.', 3.49),
+('Cappuccino', 'Dark, rich espresso lies in wait under a smoothed and stretched layer of thick foam. It is truly the height of our baristas craft.', 3.99),
+('Latte', 'Our dark, rich espresso balanced with steamed milk and a light layer of foam. A perfect milk forward warm up.', 4.35),
+('Mocha', 'We combine our rich, full-bodied espresso with bittersweet mocha sauce and steamed milk, then top it off with sweetened whipped cream. The classic coffee drink to satisfy your sweet tooth.', 3.79),
+('Pumpkin Spice Latte', 'Our signature espresso and milk are highlighted by flavor notes of pumpkin, cinnamon, nutmeg and clove to create this incredible beverage thats a fall favorite. Enjoy it topped with whipped cream and real pumpkin pie spices.', 4.99),
+('White Chocolate Mocha', 'Our signature espresso meets white chocolate sauce and steamed milk, then is finished off with sweetened whipped cream in this white chocolate delight.', 4.99),
+('Chocolate Mocha', 'Our signature espresso meets white chocolate sauce and steamed milk, then is finished off with sweetened whipped cream in this chocolate delight.', 3.59);
+
+
+---
+--- Table structure for table 'Recipes'
+---
+
+DROP TABLE IF EXISTS 'Recipes';
+CREATE TABLE 'Recipes' (
+    'menu_id' int NOT NULL,
+    'inventory_id' int NOT NULL,
+    'quantity' varchar(3) NOT NULL,
+    FOREIGN KEY ('inventory_id') REFERENCES 'Inventories' ('id'),
+    FOREIGN KEY ('menu_id') REFERENCES 'Menu' ('id')
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE 'Recipes'
+  ADD PRIMARY KEY ('menu_id','inventory_id');
+
+INSERT INTO 'Recipes' ('menu_id', 'inventory_id', 'quantity') VALUES
+(1, 1, 1),
+(1, 2, 1),
+(1, 3, 1),
+(1, 4, 2),
+(1, 5, 2),
+(2, 6, 2),
+(2, 7, 2),
+(2, 8, 2),
+(3, 9, 2),
+(3, 10, 2),
+(3, 11, 2),
+(3, 12, 2),
+(4, 13, 1),
+(4, 14, 1),
+(4, 15, 1),
+(5, 16, 1),
+(5, 17, 1),
+(6, 18, 1),
+(6, 19, 1),
+(7, 20, 1);
+
+--
+-- Table structure for table 'orders'
+--
+
+DROP TABLE IF EXISTS `Orders`;
+CREATE TABLE `Orders` (
+  `id` int(11) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `total` decimal(10,2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE `Orders`
+  ADD PRIMARY KEY (`id`);
+
+DROP TABLE IF EXISTS `Orderitems`;
+CREATE TABLE `Orderitems` (
+  `order_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  FOREIGN KEY ('order_id') REFERENCES 'Orders' ('id'),
+  FOREIGN KEY ('menu_id') REFERENCES 'Menu' ('id')
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+ALTER TABLE `Orderitems`
+  ADD PRIMARY KEY (`order_id`,`menu_id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
